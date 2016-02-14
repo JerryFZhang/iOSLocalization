fc503ac0d9eb535cacd1ec1efcd5d12909208495
zhang96 <zhang96@users.noreply.github.com>
2016-02-14 11:48:46 -0500

    Documentation and more.
    - Added a LICENSE.txt
    - Added readmemd

--- LocalizationExample/ViewController.swift
+++ LocalizationExample/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var Button: UIButton!
+    @IBOutlet weak var Segment: UISegmentedControl!
+    @IBOutlet weak var TextField: UITextField!
+    
+    
+    func populateValue(){
+    Label.text = "An example of label localization"
+    Button.setTitle("An example of button localization", forState: .Normal)
+    Segment.setTitle("First Segment", forSegmentAtIndex: 0)
+    Segment.setTitle("Second Segment", forSegmentAtIndex:1)
+    TextField.placeholder = "Placeholder Translation"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        populateValue()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
---
+++ README.md
@@ -0,0 +1,42 @@
+# SwiftWatchCounter
+Counter for the purpose of learning Swift Programming Language V2.1.
+Designed specifically for watchOS 2.
+
+By doing this I learnt:
+- Watch extensions
+- WatchKit buttons and lables.
+- Force touch menu.
+
+
+#Requirements
+Developed and tested under:
+- Swift 2.1 
+- Xcode 7.2
+- iOS 9.2.1
+- watchOS 2.1
+
+#Author 
+Jerry Zhang(zhang96)
+
+# How to use
+
+Clone or download it as a zip, open the Xcode project file and build it using the simulator or your phone and watch. Notice, it may atcually take longer period of time to get it build on your phone and watch.
+
+For iOS devices:
+- Tap to count
+- Touch and drag to count fastly
+- Tap "Reset" to clear
+
+For Apple Watches:
+- Tap to count
+- Force touch to bring the "Reset" button.
+
+
+
+#Todos 
+- Touch and hold to continuously count.
+- Responsive font sizes.
+- Coloured buttons.
+
+# License
+This component is available under MIT license.
